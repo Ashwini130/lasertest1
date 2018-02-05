f0c2f7c7124739777379f74d837a771df27ef1c9
Ashwini130 <ashwini130297@gmail.com>
2018-02-05 21:56:46 +0530

    Added Touch simulation

---
+++ app/src/main/java/com/example/lasertest1/TouchActivity.java
@@ -0,0 +1,27 @@
+package com.example.lasertest1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Ashwini on 05-02-2018.
+ */
+
+public class TouchActivity extends Activity {
+
+
+    /**
+     * Empty activity. Its only purpose is to launch the service.
+     */
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            startService(new Intent(this, MainService.class));
+
+            finish();
+        }
+}
